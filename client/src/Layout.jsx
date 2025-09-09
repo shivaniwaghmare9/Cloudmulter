7f558045d06097dea55456d2aaa619a9f7223763
shivani <shivaniw69@gmail.com>
2025-09-09 21:54:00 +0530

    layout

--- client/src/Layout.jsx
+++ client/src/Layout.jsx
@@ -1,4 +1,17 @@
 
+// import { Outlet } from 'react-router-dom';
+
+// import Topnav from "./Topnav";
+// const Layout=()=>{
+//     return(
+//         <>
+//           <Topnav/>
+//            <Outlet/>
+//         </>
+//     )
+// }
+// export default Layout;
+
 import { Outlet } from 'react-router-dom';
 
 import Topnav from "./Topnav";
