ea7c9a04e7ecb5a28646a2f03bacf46754f7c6f1
shivani <shivaniw69@gmail.com>
2025-10-13 23:50:58 +0530

    layout

--- client/src/Layout.jsx
+++ client/src/Layout.jsx
@@ -1,4 +1,30 @@
 
+// // import { Outlet } from 'react-router-dom';
+
+// // import Topnav from "./Topnav";
+// // const Layout=()=>{
+// //     return(
+// //         <>
+// //           <Topnav/>
+// //            <Outlet/>
+// //         </>
+// //     )
+// // }
+// // export default Layout;
+
+// import { Outlet } from 'react-router-dom';
+
+// import Topnav from "./Topnav";
+// const Layout=()=>{
+//     return(
+//         <>
+//           <Topnav/>
+//            <Outlet/>
+//         </>
+//     )
+// }
+// export default Layout;
+
 // import { Outlet } from 'react-router-dom';
 
 // import Topnav from "./Topnav";
