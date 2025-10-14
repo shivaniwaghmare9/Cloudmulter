34ba142456142581a67e49b2a09aaa594bf54add
shivani <shivaniw69@gmail.com>
2025-10-14 15:13:32 +0530

    app

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,26 +1,26 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Layout from "./Layout";
-import Single from "./pages/Singlefile";
-import Multer from "./pages/Multerfile";
-import Multiple from "./pages/Multiplefile";
+// import { BrowserRouter, Route, Routes } from "react-router-dom";
+// import Layout from "./Layout";
+// import Single from "./pages/Singlefile";
+// import Multer from "./pages/Multerfile";
+// import Multiple from "./pages/Multiplefile";
 
 
-const App=()=>{
-  return(
-    <>
-     <BrowserRouter>
-         <Routes>
-          <Route path="/" element={<Layout/>}>
-          <Route index element={<Single/>}/>
-          <Route path="single" element={<Single/>}/>
-          <Route path="multer" element={<Multer/>}/>
-          <Route path="multiple" element={<Multiple/>}/>
+// const App=()=>{
+//   return(
+//     <>
+//      <BrowserRouter>
+//          <Routes>
+//           <Route path="/" element={<Layout/>}>
+//           <Route index element={<Single/>}/>
+//           <Route path="single" element={<Single/>}/>
+//           <Route path="multer" element={<Multer/>}/>
+//           <Route path="multiple" element={<Multiple/>}/>
           
           
-          </Route>
-         </Routes>
-     </BrowserRouter>
-    </>
-  )
-}
-export default App;
+//           </Route>
+//          </Routes>
+//      </BrowserRouter>
+//     </>
+//   )
+// }
+// export default App;
