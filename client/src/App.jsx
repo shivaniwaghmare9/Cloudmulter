692cb489ce43baa9882deed3c31dd7c26639615c
shivani <shivaniw69@gmail.com>
2025-11-26 21:33:20 +0530

    app

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,29 +1,3 @@
-// import { BrowserRouter, Route, Routes } from "react-router-dom";
-// import Layout from "./Layout";
-// import Single from "./pages/Singlefile";
-// import Multer from "./pages/Multerfile";
-// import Multiple from "./pages/Multiplefile";
-
-
-// const App=()=>{
-//   return(
-//     <>
-//      <BrowserRouter>
-//          <Routes>
-//           <Route path="/" element={<Layout/>}>
-//           <Route index element={<Single/>}/>
-//           <Route path="single" element={<Single/>}/>
-//           <Route path="multer" element={<Multer/>}/>
-//           <Route path="multiple" element={<Multiple/>}/>
-          
-          
-//           </Route>
-//          </Routes>
-//      </BrowserRouter>
-//     </>
-//   )
-// }
-// export default App;
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Layout from "./Layout";
 import Single from "./pages/Singlefile";
